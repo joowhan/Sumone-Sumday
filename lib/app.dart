db571fbe2519d67675b6ddc52715a7ace686c316
joowhan <joy980721@gmail.com>
2023-05-16 23:42:11 +0900

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sumday/screens/mainPage.dart';
+import 'package:sumday/screens/loginPage.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => MainPage(),
+      },
+      initialRoute: '/login',
+      home: LoginPage(),
+      onGenerateRoute: _getRoute,
+    );
+  }
+  Route<dynamic>? _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sumday/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
