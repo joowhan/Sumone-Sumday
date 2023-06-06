f3dcbb65803ae7e2f707b439c57730d31912fe8d
wNemon <testemail@testemail.com>
2023-06-06 12:11:29 +0900

    fix : Modify the diary list layout

--- lib/models/diary_model.dart
+++ lib/models/diary_model.dart
@@ -3,11 +3,13 @@ class Diary {
     required this.id,
     required this.date,
     required this.tags,
+    required this.favorite,
   });
 
   final int id;
   final DateTime date;
   final List<String> tags;
+  final bool favorite;
 
   String get assetName => 'sorry.png';
 }
--- lib/screens/diaries.dart
+++ lib/screens/diaries.dart
@@ -6,6 +6,7 @@ import 'package:intl/intl.dart';
 
 import 'package:sumday/models/diary_model.dart';
 import 'package:sumday/models/diary_data.dart';
+
 class Diaries extends StatefulWidget {
   const Diaries({Key? key}) : super(key: key);
 
@@ -32,57 +33,89 @@ class _DiariesState extends State<Diaries> {
 
     return diaries.map((diary) {
       return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
         clipBehavior: Clip.antiAlias,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 18 / 11,
-                child: Image.asset(
-                  'assets/${diary.assetName}',
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(14.0, 12.0, 14.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        _joinWithHash(diary.tags),
-                        style: TextStyle(
-                            color: Colors.purple.shade400,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 12.0),
-                      Text(
-                        _formattedDate(diary.date),
-                        style: theme.textTheme.titleSmall,
-                      ),
-                    ],
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 12 / 10,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(4.5, 4.5, 4.5, 4.5),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    // border: Border.all(
+                    //   color: Colors.black,
+                    //   width: 1.0,
+                    // ),
+                  ),
+                  child: Image.asset(
+                    'assets/${diary.assetName}',
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(14.0, 12.0, 14.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const SizedBox(height: 12.0),
+                    Text(
+                      _joinWithHash(diary.tags),
+                      style: const TextStyle(
+                          color: Color.fromARGB(0xff, 0x13, 0x67, 0x50),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10.0),
+                    Text(
+                      _formattedDate(diary.date),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                    const SizedBox(height: 12.0),
+                    Text(
+                      // _joinWithHash('diary.tags'),
+                      '여기는 일기 내용이 오는 곳이에요. 일기 내용은 나중에 디비에서 가져와야해요.',
+                      style: TextStyle(
+                          color: Colors.grey.shade900,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 4.5, 4.5, 0.0),
+              child: Icon(
+                diary.favorite ? Icons.favorite : Icons.favorite_border,
+                color: diary.favorite ? Colors.red : null,
+              ),
+            ),
+          ],
         ),
       );
     }).toList();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: 1,
         crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
+        mainAxisSpacing: 3,
+        // padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 2.5,
         children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
