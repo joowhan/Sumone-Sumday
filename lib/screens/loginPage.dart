c2eb5e978442091d70212c920ecfdc940eebb488
Ruru <adskus3412@gmail.com>
2023-06-27 05:41:52 +0900

    ✨ Feat : 추가 뒤로가기 시 알림창, 이동 시 필요없는 탭 닫기
    - loginPage.dart : 뒤로가기 시 알림창, 로그인 성공 시 로그인창 닫기
    - mainPage.dart : 뒤로가기 시 알림창
    - ai_writeDiary.dart : 일기 생성 페이지 이동 시 설문창 닫기(뒤로가기 누르면 바로 메인)

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -24,7 +24,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   Future<bool> permission() async {
     Map<Permission, PermissionStatus> status =
-    await [Permission.location].request(); // [] 권한배열에 권한을 작성
+        await [Permission.location].request(); // [] 권한배열에 권한을 작성
 
     if (await Permission.location.isGranted) {
       return Future.value(true);
@@ -33,103 +33,128 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     LoginProvider loginProvider = Provider.of(context, listen: true);
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 200.0),
-            Image.asset(
-              "assets/main_logo.png",
-              width: 300,
-              height: 300,
-            ),
-            SizedBox(height: 10.0),
-            OutlinedButton.icon(
-              onPressed: () async {
-                bool hasPermission = await permission(); // 위치 권한 요청
+    // 뒤로가기 누를 경우 종료 물어보는 부분
+    return WillPopScope(
+      onWillPop: () async {
+        return await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: Text('정말 종료하겠습니까?'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text('확인'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text('취소'),
+                        ),
+                      ],
+                    )) ??
+            false;
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 200.0),
+              Image.asset(
+                "assets/main_logo.png",
+                width: 300,
+                height: 300,
+              ),
+              SizedBox(height: 10.0),
+              OutlinedButton.icon(
+                onPressed: () async {
+                  bool hasPermission = await permission(); // 위치 권한 요청
 
-                if (hasPermission) {
-                  try {
-                    await loginProvider.signInWithGoogle(); // 구글 로그인
-                    print("Google Login Success!!");
-                    Navigator.pushNamed(context, '/home');
-                  } catch (e) {
-                    if (e is FirebaseAuthException) {
-                      print(e.message!);
+                  if (hasPermission) {
+                    try {
+                      await loginProvider.signInWithGoogle(); // 구글 로그인
+                      print("Google Login Success!!");
+                      if (!mounted) return; // 업으면 아랫줄 에러 발생
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, '/home', (route) => false);
+                    } catch (e) {
+                      if (e is FirebaseAuthException) {
+                        print(e.message!);
+                      }
                     }
+                  } else {
+                    print('위치 권한이 필요합니다.');
                   }
-                } else {
-                  print('위치 권한이 필요합니다.');
-                }
-              },
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(width: 2, color: Color(0xFFF4C54F)),
-                fixedSize: const Size(300, 55),
-                backgroundColor: Colors.white,
-              ),
-              label: Text(
-                "Google로 로그인 하기",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color(0xFFF4C54F),
+                },
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(width: 2, color: Color(0xFFF4C54F)),
+                  fixedSize: const Size(300, 55),
+                  backgroundColor: Colors.white,
+                ),
+                label: Text(
+                  "Google로 로그인 하기",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color(0xFFF4C54F),
+                  ),
+                ),
+                icon: Image.asset(
+                  "assets/google.png",
+                  height: 30,
+                  width: 30,
                 ),
               ),
-              icon: Image.asset(
-                "assets/google.png",
-                height: 30,
-                width: 30,
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () async {
-                bool hasPermission = await permission(); // 위치 권한 요청
-                if (hasPermission) {
-                  try {
-                    await loginProvider.signInWithAnonymous(); // 익명 로그인
-                    print("Anonymous Login Success!!");
-                    Navigator.pushNamed(context, '/home');
-                  } catch (e) {
-                    if (e is FirebaseAuthException) {
-                      print(e.message!);
+              OutlinedButton(
+                onPressed: () async {
+                  bool hasPermission = await permission(); // 위치 권한 요청
+                  if (hasPermission) {
+                    try {
+                      await loginProvider.signInWithAnonymous(); // 익명 로그인
+                      print("Anonymous Login Success!!");
+                      // Navigator.pushNamed(context, '/home');
+                      if (!mounted) return; // 업으면 아랫줄 에러 발생
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, '/home', (route) => false);
+                    } catch (e) {
+                      if (e is FirebaseAuthException) {
+                        print(e.message!);
+                      }
                     }
+                  } else {
+                    print("위치 권한이 필요합니다."); // 위치 권한이 없는 경우 처리
                   }
-                } else {
-                  print("위치 권한이 필요합니다."); // 위치 권한이 없는 경우 처리
-                }
-              },
-              child: Text(
-                '익명 로그인',
-                style: TextStyle(
-                  color: Color(0xFFF4C54F),
+                },
+                child: Text(
+                  '익명 로그인',
+                  style: TextStyle(
+                    color: Color(0xFFF4C54F),
+                  ),
                 ),
               ),
-            ),
-            // OutlinedButton(onPressed: () async{
-            //   try {
-            //     await loginProvider.signInWithAnonymous(); // 구글 로그인
-            //     print("Anonymous Login Success!!");
-            //
-            //     Navigator.pushNamed(context, '/home');
-            //   } catch (e) {
-            //     if (e is FirebaseAuthException) {
-            //       print(e.message!);
-            //     }
-            //   }
-            //   Navigator.pushNamed(context, '/home');
-            // },
-            //     child: Text('익명 로그인',
-            //       style: TextStyle(
-            //         color: Color(0xFFF4C54F), // 여기에 색상을 추가하세요
-            //       ),
-            //     ),
-            // ),
-          ],
+              // OutlinedButton(onPressed: () async{
+              //   try {
+              //     await loginProvider.signInWithAnonymous(); // 구글 로그인
+              //     print("Anonymous Login Success!!");
+              //
+              //     Navigator.pushNamed(context, '/home');
+              //   } catch (e) {
+              //     if (e is FirebaseAuthException) {
+              //       print(e.message!);
+              //     }
+              //   }
+              //   Navigator.pushNamed(context, '/home');
+              // },
+              //     child: Text('익명 로그인',
+              //       style: TextStyle(
+              //         color: Color(0xFFF4C54F), // 여기에 색상을 추가하세요
+              //       ),
+              //     ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/mainPage.dart
+++ lib/screens/mainPage.dart
@@ -78,94 +78,114 @@ class _MainPageState extends State<MainPage> {
     final screenHeight = MediaQuery.of(context).size.height;
     final appBarHeight = screenHeight * 0.15; // 화면 높이의 10%
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Image.asset('assets/small_logo.png'),
-          onPressed: () {
-            print('home_icon is clicked');
-          },
+    return WillPopScope(
+      onWillPop: () async {
+        return await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: Text('정말 종료하겠습니까?'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text('확인'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text('취소'),
+                        ),
+                      ],
+                    )) ??
+            false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Image.asset('assets/small_logo.png'),
+            onPressed: () {
+              print('home_icon is clicked');
+            },
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print('search Icon');
+                print('${ModalRoute.of(context)?.settings.name}');
+              },
+              icon: const Icon(
+                Icons.search,
+                color: Colors.black38,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                print('alarm Icon');
+              },
+              icon: const Icon(
+                Icons.alarm,
+                color: Colors.black38,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                print('edit Icon');
+              },
+              icon: const Icon(
+                Icons.edit,
+                color: Colors.black38,
+              ),
+            ),
+          ],
+          backgroundColor: Colors.white,
+          elevation: 0.25,
         ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              print('search Icon');
-              print('${ModalRoute.of(context)?.settings.name}');
-            },
-            icon: const Icon(
-              Icons.search,
-              color: Colors.black38,
+        body: Center(
+          child: _widgetOptions[_selectedIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              print('alarm Icon');
-            },
-            icon: const Icon(
-              Icons.alarm,
-              color: Colors.black38,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_books_rounded),
+              label: '목록',
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              print('edit Icon');
-            },
-            icon: const Icon(
-              Icons.edit,
-              color: Colors.black38,
+            // BottomNavigationBarItem(
+            //   icon: Icon(
+            //     Icons.add_circle_outline,
+            //     size: 45,
+            //   ),
+            //   label: '',
+            // ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_border),
+              label: '북마크',
             ),
-          ),
-        ],
-        backgroundColor: Colors.white,
-        elevation: 0.25,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit_note_sharp),
+              label: '교환일기',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: '설정',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color(0xff136750),
+          backgroundColor: Colors.white,
+          onTap: _onItemTapped,
+        ),
+        floatingActionButton: AnimatedFloatingActionButton(
+            //Fab list
+            fabButtons: <Widget>[float1(), float2()],
+            key: key,
+            colorStartAnimation: Colors.blue,
+            colorEndAnimation: Colors.red,
+            animatedIconData: AnimatedIcons.menu_close //To principal button
+            ),
       ),
-      body: Center(
-        child: _widgetOptions[_selectedIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books_rounded),
-            label: '목록',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(
-          //     Icons.add_circle_outline,
-          //     size: 45,
-          //   ),
-          //   label: '',
-          // ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark_border),
-            label: '북마크',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit_note_sharp),
-            label: '교환일기',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '설정',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xff136750),
-        backgroundColor: Colors.white,
-        onTap: _onItemTapped,
-      ),
-      floatingActionButton: AnimatedFloatingActionButton(
-          //Fab list
-          fabButtons: <Widget>[float1(), float2()],
-          key: key,
-          colorStartAnimation: Colors.blue,
-          colorEndAnimation: Colors.red,
-          animatedIconData: AnimatedIcons.menu_close //To principal button
-          ),
     );
   }
 }
